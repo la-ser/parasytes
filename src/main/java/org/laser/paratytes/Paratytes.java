444febf675858f8a0d9fa8c63f0c17c338d505a2
la-ser <77540680+la-ser@users.noreply.github.com>
2024-03-30 22:38:32 +0100

    lsr

--- src/main/java/org/laser/paratytes/Paratytes.java
+++ src/main/java/org/laser/paratytes/Paratytes.java
@@ -1,5 +1,9 @@
 package org.laser.paratytes;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Paratytes extends JavaPlugin {
@@ -8,6 +12,26 @@ public final class Paratytes extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+
+        if (event.getWhoClicked() instanceof Player) {
+
+            Player player = (Player) event.getWhoClicked();
+
+            if (event.getClickedInventory() != null) {
+
+                if (event.getRawSlot() < event.getView().getTopInventory().getSize()) event.setCancelled(true);
+
+                if (event.getCurrentItem() != null && event.getCurrentItem().getType() == Material.SCUTE) {
+
+                    player.sendMessage("Lets begin the tail!");
+
+                }
+            }
+        }
     }
 
     @Override
