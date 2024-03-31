e5335442c226624b25abff4dca79c687d6ac0c34
Ardachen <161491939+Ardachen@users.noreply.github.com>
2024-03-31 23:46:56 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/laser/paratytes/Paratytes.java
+++ src/main/java/org/laser/paratytes/Paratytes.java
@@ -1,8 +1,11 @@
+package org.laser.paratytes;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
