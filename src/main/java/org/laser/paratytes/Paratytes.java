d565497004b1f90120c78b836f2c6c9a5761c3bf
Ardachen <161491939+Ardachen@users.noreply.github.com>
2024-03-31 23:16:36 +0200

    lsr

--- src/main/java/org/laser/paratytes/Paratytes.java
+++ src/main/java/org/laser/paratytes/Paratytes.java
@@ -1,19 +1,54 @@
 package org.laser.paratytes;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Paratytes extends JavaPlugin {
+import java.util.HashMap;
+import java.util.UUID;
+
+public final class Paratytes extends JavaPlugin implements Listener {
+
+    private HashMap<UUID, ItemStack[]> savedInventories = new HashMap<>();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    // saves inventory + player gets lobby inventory
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+
+        saveInventory(player);
+
+        player.getInventory().clear();
+
+        player.getInventory().setItem(0, new ItemStack(Material.SCUTE, 1));
 
     }
 
+    // save / restore inventory
+    private void saveInventory(Player player) { savedInventories.put(player.getUniqueId(), player.getInventory().getContents()); }
+    private void restoreInventory(Player player) {
+        if (savedInventories.containsKey(player.getUniqueId())) {
+            player.getInventory().setContents(savedInventories.get(player.getUniqueId()));
+            savedInventories.remove(player.getUniqueId()); // Inventar aus der Speicherliste entfernen
+        }
+    }
+
+    // start base, join base, return to your base
+    @EventHandler
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
 
@@ -27,9 +62,48 @@ public final class Paratytes extends JavaPlugin {
 
                 if (event.getCurrentItem() != null && event.getCurrentItem().getType() == Material.SCUTE) {
 
-                    player.sendMessage("Lets begin the tail!");
+                    openBaseInventory(player);
 
+                } else {
+                    handleBaseOperations(player, event.getCurrentItem());
                 }
+            }
+        }
+    }
+
+    private void openBaseInventory(Player player) {
+
+        Inventory baseInventory = Bukkit.createInventory(player, org.bukkit.event.inventory.InventoryType.HOPPER, "Base Operations Menu");
+
+        ItemStack establishBaseItem = new ItemStack(Material.GHAST_TEAR);
+        ItemMeta establishBaseMeta = establishBaseItem.getItemMeta();
+        establishBaseMeta.setDisplayName("Establish a base");
+        establishBaseItem.setItemMeta(establishBaseMeta);
+        baseInventory.setItem(0, establishBaseItem);
+
+        ItemStack joinBaseItem = new ItemStack(Material.ENDER_EYE);
+        ItemMeta joinBaseMeta = joinBaseItem.getItemMeta();
+        joinBaseMeta.setDisplayName("Join base");
+        joinBaseItem.setItemMeta(joinBaseMeta);
+        baseInventory.setItem(2, joinBaseItem);
+
+        ItemStack returnToBaseItem = new ItemStack(Material.COMPASS);
+        ItemMeta returnToBaseMeta = returnToBaseItem.getItemMeta();
+        returnToBaseMeta.setDisplayName("Return to base");
+        returnToBaseItem.setItemMeta(returnToBaseMeta);
+        baseInventory.setItem(4, returnToBaseItem);
+
+        player.openInventory(baseInventory);
+    }
+
+    private void handleBaseOperations(Player player, ItemStack clickedItem) {
+
+        if (clickedItem != null && clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
+            String itemName = clickedItem.getItemMeta().getDisplayName();
+
+            if (itemName.equals("Establish a base")) {
+            } else if (itemName.equals("Join base")) {
+            } else if (itemName.equals("Return to base")) {
             }
         }
     }
@@ -38,4 +112,104 @@ public final class Paratytes extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
